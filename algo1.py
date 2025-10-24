e5a041e6204ccdbbb7bc843ed25b1c1005f08c48
Daniel Hernandez <143033040+dh21704@users.noreply.github.com>
2025-10-24 11:53:43 -0700

    Update algo1.py

--- algo1.py
+++ algo1.py
@@ -1,33 +1,30 @@
 def algo1(arr):
     n = len(arr)
     half = n // 2
-    l = arr[:half]
-    r = arr[half:]
-    return l, r
-
-
-def solve_right(arr):
+    
+    selected_right = []
+    selected_left = []
+    
     last = arr[-1]
-    selected = []
-    for val in reversed(arr):
-        if val >= last:
-            selected.append(val)
-            last = val
-    return selected
-
-def solve_left(arr):
     first = arr[0]
-    selected = []
-    for i in arr:
-        if i >= first:
-            selected.append(i)
-            first = i 
+    
+    for i in range(len(arr)):
+        if arr[i] >= first:
+            selected_left.append(i)
+            first = arr[i]
+            
 
+    for i in range(len(arr) - 1, -1, -1):
+        if arr[i] >= last:
+            selected_right.append(i)
+            last = arr[i]
+            
+
+    
+    return selected_left, selected_right
+    
+    
 arr = [3, 7, 8, 3, 6, 1]
 left, right = algo1(arr)
-print('left:', left)
-print('right:', right)
-
-visible_right = solve_right(right)
-visible_left = solve_left(left)
-print('visible_right:', visible_right)
+print(left)
+print(right)
