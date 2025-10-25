02d727754f5e2d0a81b693aecfe480d566a38a50
Daniel Hernandez <143033040+dh21704@users.noreply.github.com>
2025-10-25 16:16:01 -0700

    Update algo2.py

--- algo2.py
+++ algo2.py
@@ -1,16 +1,17 @@
-
-# sort
+#create list of  tuples
 tup = [(1, 3), (2, 5), (4, 6), (6, 7), (5, 9), (8, 9)]
+
+#sorts the tupples based on the second element of each tuple 
 tup.sort(key=lambda x: x[1])
 
-selected = []
-last = 0
+selected = [] #empty list to store tuples 
+last = 0 #keep track of last element 
 
 
-for x, y in tup:
-    if x >= last:
-        selected.append((x, y))
+for x, y in tup: #loop through each tupple first and second key 
+    if x >= last: #if first is greater than the last element we iterated through 
+        selected.append((x, y)) #add the tuple to the the list 
         last = y
 
 
-print(selected)
+print(selected) #print the list 
